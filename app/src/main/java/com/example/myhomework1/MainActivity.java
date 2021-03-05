0ac392c4179f149b679257f29667ea13aa91d831
SergTaranenko <rates-13@inbox.ru>
2021-03-05 05:39:10 +0300

    кнопка для перехода на второй макет не работает, не сообразил как связать макеты

--- app/src/main/java/com/example/myhomework1/MainActivity.java
+++ app/src/main/java/com/example/myhomework1/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.myhomework1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.actvity2_main);
     }
+
+    public void Click(View view) {
+                    //Создаем переход:
+                Intent intent=new Intent(MainActivity.this,R.layout.activity_3);
+                //Запускаем его при нажатии:
+                startActivity(intent);
+            }
+
 }
